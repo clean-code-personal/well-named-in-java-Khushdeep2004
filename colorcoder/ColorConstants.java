d0ae96269319a0fdc4a07ab0e9909cd8b6195c59
Khushdeep2004 <99407262+Khushdeep2004@users.noreply.github.com>
2024-10-28 10:44:06 +0530

    Update ColorConstants.java

--- colorcoder/ColorConstants.java
+++ colorcoder/ColorConstants.java
@@ -3,10 +3,10 @@ package colorcoder;
 public class ColorConstants {
    public final static String MajorColorNames[] = {
         "White", "Red", "Black", "Yellow", "Violet"
-    }
+    };
    public final static int numberOfMajorColors = MajorColorNames.length;
    public final static String MinorColorNames[] = {
         "Blue", "Orange", "Green", "Brown", "Slate"
-    }
+    };
     public final static int numberOfMinorColors = MinorColorNames.length;
 }
