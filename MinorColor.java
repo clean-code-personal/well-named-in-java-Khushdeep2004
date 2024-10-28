453ad57d2091362a2be4afa01889793255771285
Khushdeep2004 <99407262+Khushdeep2004@users.noreply.github.com>
2024-10-28 09:51:20 +0530

    Update fuction MinorColor.java

--- MinorColor.java
+++ MinorColor.java
@@ -1,24 +1,25 @@
 package colorcoder;
 
-public enum MinorColor implements Color {
+public enum MinorColor {
     BLUE(0),
     ORANGE(1),
     GREEN(2),
     BROWN(3),
     SLATE(4);
 
-    private int index;
-
-    MinorColor(int index) {
-        this.index = index;
-    }
-
-    @Override
-    public int getIndex() {
-        return index;
-    }
-
-    public static MinorColor fromIndex(int index) {
-        return Color.fromIndex(index, MinorColor.values());
-    }
+     private int index;
+        private MinorColor(int index) {
+            this.index = index;
+        }
+        int getIndex() {
+            return index;
+        }
+        public static MinorColor fromIndex(int index) {
+            for(MinorColor color: MinorColor.values()) {
+                if(color.getIndex() == index) {
+                    return color;
+                }
+            }
+            return null;
+}
 }
